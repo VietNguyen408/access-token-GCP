0ef89350a9d7b828549597aa2bf84b5ffb592a66
Viet Nguyen <nguyentrungviet04082001@gmail.com>
2021-11-17 15:41:08 +0100

    first commit

--- get_gcp_token.sh
+++ get_gcp_token.sh
@@ -3,31 +3,31 @@
 #This program will get a GCP token to log in to Google Cloud Platform
 
 #Get Keycloak access token through curl command
-curl -d client_id=admin-cli -d client_secret=8e2f9510-f34c-4dec-a01a-10d3d046412a -d username=admin -d password=admin -d grant_type=password http://127.0.0.1:8081/auth/realms/master/protocol/openid-connect/token | jq . >keycloak-access-token.json
+curl -d 'client_id=admin_cli' -d 'client_secret=YOUR_CLIENT_SERCRET' -d 'username=YOUR_USERNAME' -d 'password=YOUR_PASSWORD' -d 'grant_type=password' 'http://localhost:8080/auth/realms/YOUR_REALM_NAME/protocol/openid-connect/token' | jq . >keycloak-access-token.json
 KEYCLOAK_ACESS_TOKEN=$(jq -r .access_token keycloak-access-token.json)
 
 #Authorize in Keycloak and get gcp-token in user Viet
 curl \
 	-H "Authorization: bearer $KEYCLOAK_ACESS_TOKEN" \
-	"http://127.0.0.1:8081/auth/admin/realms/master/users/7676dcd5-03ce-446e-8045-7ba0dbf6e558" | jq . >gcp-token-keycloak.json
+	"http://localhost:8080/auth/admin/realms/YOUR_REALM_NAME/users/YOUR_USER_ID" | jq . >gcp-token-keycloak.json
 
 #Parse JSON to get gcp-refresh-token
 GCP_REFRESH_TOKEN=$(jq -r '.attributes["GCP-refresh-token"][]' gcp-token-keycloak.json)
 echo "The GCP refresh token is $GCP_REFRESH_TOKEN"
+
 #Exchange GCP refresh token for a new access token
-APP_CLIENT_ID="790131216438-2g4tmjsldi9a861m59uple5d1bvigfft.apps.googleusercontent.com"
-APP_CLIENT_SERCRET="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"
+APP_CLIENT_ID="YOUR_APP_CLIENT_ID"
+APP_CLIENT_SERCRET="YOUR_APP_CLIENT_SERCRET"
 
 curl \  --request POST \  --data "client_id=$APP_CLIENT_ID&client_secret=$APP_CLIENT_SERCRET&refresh_token=$GCP_REFRESH_TOKEN&grant_type=refresh_token" "https://accounts.google.com/o/oauth2/token" | jq . >gcp-new-access-token.json
 
 GCP_NEW_ACCESS_TOKEN=$(jq -r .access_token gcp-new-access-token.json)
+
+#Save the GCP_NEW_ACCESS_TOKEN to a file to use it in Jenkins pipeline
 touch GCP_NEW_ACCESS_TOKEN.txt
 destdir=GCP_NEW_ACCESS_TOKEN.txt
 
 if [ -f "$destdir" ]; then
 	echo "$GCP_NEW_ACCESS_TOKEN" >"$destdir"
 fi
-
 echo "The new GCP access token is $GCP_NEW_ACCESS_TOKEN"
-
-#https://stackoverflow.com/questions/66940072/get-google-oauth2-access-token-using-only-curl
